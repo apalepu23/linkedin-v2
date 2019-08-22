0efee38897ce19144cdb4c3ffbb76aa102b8ea57
Aditya Palepu <apalepu23@gmail.com>
2019-08-22 16:34:27 -0400

    log

--- lib/linked_in/api_resource.rb
+++ lib/linked_in/api_resource.rb
@@ -55,7 +55,7 @@ module LinkedIn
 
     def get(path, options={})
       url, params, headers = prepare_connection_params(path, options)
-
+      puts "URL", url
       response = @connection.get(url, params, headers)
       Mash.from_json(response.body)
     end
